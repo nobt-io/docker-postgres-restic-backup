fd1c2f22af7af197746ff9410a2e82e7160c01ec
Thomas Eizinger <thomas@eizinger.io>
2022-09-05 06:00:59 +1000

    Make it a config haiku

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-crond \
-	-f \ # Run crond in foreground to keep container running
-	-d 8 # Log messages to stderr
+crond -f \ # Run crond in foreground to keep container running
+      -d 8 # Log messages to stderr
